15dbef29c3820a8a09b9c3dd65457d8ef33c80d5
marceldonin <mdonin@gmail.com>
2021-05-19 15:46:09 -0300

    aula12

---
+++ aula12/ex11.js
@@ -0,0 +1,17 @@
+var idade = 15
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16)
+{
+    console.log('Não Vota')
+} else if (idade < 18)
+        {
+            console.log('Voto opcional')           
+        } else if (idade >= 65)
+                {
+                    console.log('Voto opcional')
+                } else
+                    {
+                        console.log('Voto Obrigatório')
+                    }   
+            
+   
